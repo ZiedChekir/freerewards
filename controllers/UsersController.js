01990647a8fd7ec19f76055d9ff6d6c8f9310176
ZiedChekir <ziedchekir1@gmail.com>
2018-03-18 13:50:22 +0100

    fix

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -57,13 +57,38 @@ module.exports = {
             return res.redirect(`/user/register?name=${name}&username=${username}&email=${email}`)
 
         }
-        if (password != password2) {
-            req.flash('errors', "passwords don't match")
-            return res.redirect('/user/register')
-
-        }
+ 
+       
+            if (process.env.NODE_ENV == 'production') {
+                var recapatcha = req.body['g-recaptcha-response']
+                if (recapatcha == '' || recapatcha == null || recapatcha == undefined) {
+                    
+                    req.flash('errors', 'Make sure to check recapatcha')
+                    return res.redirect(`/user/register?name=${name}&username=${username}&email=${email}`)
+            
+                }
+                var secretKey = "6LfGQ00UAAAAAAtDN5vTsav_EiQ6Kj8Xsb8vcgV-"
+                var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+            
+                console.log('before request')
+                request(verificationUrl, function (error, res, body) {
+                    if (body.success !== undefined && !body.success) {
+                        debug('success is false')
+                        req.flash('errors', 'something went wrong with recapatcha!')
+                        return res.redirect(`/user/register?name=${name}&username=${username}&email=${email}`)
+            
+                    }
+                    if (body.success) {
+                        req.flash('success', 'you are now logged in!')
+                        next()
+                    }
+            
+                })
+            } 
+            // else {
+            //     next()
+            // }
         
-   
         try {
             var emailExist = await userOperation.queryByEmail(email)
             var usernameExist = await userOperation.queryByUsername(username)
